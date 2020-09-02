319fd2efca8f3b4fda13cd9b87cc10beff098f33
Gabriel <gabrielclimb@gmail.com>
2020-09-02 17:21:43 -0300

    fix imports

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import base64
-from io import BytesIO, StringIO
+from io import StringIO
 
-import pandas as pd
 import streamlit as st
-from streamlit import dataframe
 from handlers.dbcheck import validCollar
 
 
@@ -28,11 +26,10 @@ def main():
 
     if st.sidebar.button('Process'):
         if collar:
-
             # df_collar = pd.read_csv(StringIO(collar.read()))
             # st.dataframe(df_collar)
-            bhid='BHID'
-            xyzcol=['XCOLLAR', 'YCOLLAR', 'ZCOLLAR']
+            bhid = 'BHID'
+            xyzcol = ['XCOLLAR', 'YCOLLAR', 'ZCOLLAR']
             df_csv = validCollar(bhid, xyzcol, StringIO(collar.read()))
             st.markdown(get_csv_download_link(df_csv, 'error_collar'),
                         unsafe_allow_html=True)
@@ -53,20 +50,6 @@ def get_csv_download_link(dataframe, filename):
     return ('<p style="text-align:center;">'
             f'<a href="data:application/octet-stream;base64,{b64.decode()}" '
             f'download="{filename}.csv">Download CSV file</a></p>')
-
-
-def to_csv(dataframe):
-    """[summary]
-    Arguments:
-        dataframe {pandas.DataFrame} -- pandas dataframe
-    Returns:
-        bytes -- dataframe to csv to bytes
-    """
-    output = BytesIO()
-    dataframe.to_csv(output, index=False)
-    processed_data = output.getvalue()
-    return processed_data
-
 
 if __name__ == '__main__':
     main()
