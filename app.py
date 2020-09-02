5a3f3b51129028145fb5116a2bd37ec5deb1c04d
Gabriel <gabrielclimb@gmail.com>
2020-09-02 16:45:06 -0300

    download func

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import base64
+
 import pandas as pd
 import streamlit as st
 from handlers.dbcheck import validCollar
@@ -24,6 +26,23 @@ def main():
 
     if st.sidebar.button('Process'):
         st.write('clicked')
+
+
+def get_csv_download_link(csv, filename):
+    """
+    Function that creates a link to download file
+    Arguments:
+        csv {não sei ainda} -- output from function with errors
+    Returns:
+        str -- html that creates hyperlink to file in cache
+    """
+
+    val = bytes(csv, 'utf-8')
+    b64 = base64.b64encode(val)
+    return ('<p style="text-align:center;">'
+            f'<a href="data:application/octet-stream;base64,{b64.decode()}" '
+            f'download="{filename}.csv">Download CSV file</a></p>')
+
 
 if __name__ == '__main__':
     main()
